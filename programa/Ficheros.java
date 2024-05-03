51359d60c71d9a95c74aca0b55acd30606146eba
Juanies <juanromeroviudez@gmail.com>
2024-05-03 22:43:09 +0200

    Ficheros

--- programa/Ficheros.java
+++ programa/Ficheros.java
@@ -1,11 +1,59 @@
 package programa;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Ficheros {
 
     public static File getFicherousuarios(){
         return new File("usuarios.dat");
+    }
+
+    public static ArrayList<Object> leerObjectoFichero() throws IOException, ClassNotFoundException {
+
+        ObjectInputStream  oi = new ObjectInputStream(new FileInputStream(getFicherousuarios()));
+        ArrayList<Object> res = new ArrayList<>();
+
+        while (oi.available() > 0){
+            Object ob = oi.readObject();
+            res.add(ob);
+        }
+
+        oi.close();
+        return res;
+
+    }
+
+
+    public static boolean insertarObjetosFichero(File fichero, ArrayList<Object> objetosInsertar)  {
+
+        Boolean res = true;
+
+
+
+       try {
+
+           ArrayList<Object> objetos = leerObjectoFichero();
+
+
+           for(Object datos : objetosInsertar){
+               objetos.add(datos);
+           }
+
+           ObjectOutputStream Oout = new ObjectOutputStream(new FileOutputStream(getFicherousuarios()));
+
+           for(Object datos : objetos){
+               Oout.writeObject(datos);
+           }
+
+           Oout.close();
+
+       }catch (Exception e){
+
+           res = false;
+           e.printStackTrace();
+       }
+        return res;
     }
 
 }
