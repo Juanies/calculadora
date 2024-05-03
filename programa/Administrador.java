16f0e51eba262e48d3c9600288477ad7caf50063
Juan Romero <80675013+Juanies@users.noreply.github.com>
2024-05-03 17:20:59 +0200

    Update Administrador.java

--- programa/Administrador.java
+++ programa/Administrador.java
@@ -1,7 +1,23 @@
 package programa;
 
 public class Administrador extends Usuario{
-    public Administrador(String nombre, String contrasena, int id, String rol) {
-        super(nombre, contrasena, id, rol);
+    public Administrador(String nombre, String contrasena, String rol) {
+        super(nombre, contrasena, rol);
     }
+
+    public static boolean crearNuevoUsuario(String nombre, String contrasena, String rol){
+        Object usuario = new Object();
+        boolean res = false;
+        if (rol.equals("Administrador")){
+            usuario = new Administrador(nombre, contrasena, rol);
+            res = true;
+        }else if (rol.equals("Usuario")){
+            usuario = new Usuario(nombre, contrasena, rol);
+            res = true;
+        }
+        return res;
+    }
+
+
+
 }
